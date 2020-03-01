fa9a047eed5b13f32b3e15ec01024e473013d8a6
hamadi aziz <aziz@hamadis-Mac.local>
2020-03-01 16:53:13 +0100

    Initial project setup

---
+++ EventBus/EventBus.h
@@ -0,0 +1,19 @@
+//
+//  EventBus.h
+//  EventBus
+//
+//  Created by hamadi aziz on 01/03/2020.
+//  Copyright © 2020 hamadi aziz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EventBus.
+FOUNDATION_EXPORT double EventBusVersionNumber;
+
+//! Project version string for EventBus.
+FOUNDATION_EXPORT const unsigned char EventBusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EventBus/PublicHeader.h>
+
+
---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'EventBus' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  pod 'BlueSocket'
+  pod 'SwiftyJSON'
+
+  # Pods for EventBus
+
+end
